469bd6e67fb8c807e1a9afb39aec2b099380da38
eduard <webeduard@yandex.ru>
2017-03-02 15:28:17 +0300

    merge scroll-js

--- gulpfile.js
+++ gulpfile.js
@@ -92,6 +92,7 @@ gulp.task('scripts', function() {
     './app/libs/scrollMagic/ScrollMagic.min.js',
     './app/libs/greensock/TweenMax.min.js',
     './app/libs/scrollMagic/animation.gsap.min.js',
+    './app/libs/greensock/ScrollToPlugin.min.js',
     './app/libs/scrollMagic/debug.addIndicators.min.js'
 	])
 	.pipe(concat('libs.js'))
